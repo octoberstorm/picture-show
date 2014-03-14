27f3d1dbf7b156c0efac64fc535bf4294b78314d
octoberstorm <storm.october@gmail.com>
2014-03-14 15:39:42 +0700

    Change photo filename to prevent duplicate

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,4 +4,12 @@ class ImageUploader < CarrierWave::Uploader::Base
   version :thumb do
     process :resize_to_limit => [200, 200]
   end
+
+  def filename
+    if original_filename.present?
+      "#{DateTime.now.to_i}_#{original_filename}"
+    else
+      super
+    end
+  end
 end
